f5feef0016cffeafcbc1ffba352d74b2940ebdf7
WahNeyDah <122383343+WahNeyDah@users.noreply.github.com>
2023-05-02 14:24:03 -0500

    added death

---
+++ Platform/Assets/scrip/Player_life.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_life : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+    private void Die()
+    {
+        anim.SetTrigger("Death");
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+}
